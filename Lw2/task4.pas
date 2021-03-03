0ba4f2cfd7d58023c54de58a4e9f8f4a6cfb65e3
Alexey <alexdragh.09@gmail.com>
2021-03-03 17:55:32 +0300

    Изменения в 4 лабе

--- Lw2/task4.pas
+++ Lw2/task4.pas
@@ -2,8 +2,17 @@ PROGRAM SarahRevere(INPUT, OUTPUT);
 USES
   DOS;
 VAR
-  QueryStr: STRING
+  QueryStr: STRING;
+
 FUNCTION GetQueryStringParameter(Key: STRING): STRING;
+VAR
+  KeyLength, PosKey, Pos&: INTEGER;
+BEGIN
+  DeltaPos := 0;
+  KeyLength := LENGTH(Key);
+  PosKey := POS(Key, QueryStr);
+  
+END;
 
 BEGIN {Response}
   WRITELN('Content-Type: text/plain');
